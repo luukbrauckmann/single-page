e0f777a3d1fb2db6e72322f31cdbbd061a173ab2
Luuk <luukbrauckmann@gmail.com>
2022-11-19 17:52:30 +0100

    Update router Gebruik van hash

--- scripts/router.js
+++ scripts/router.js
@@ -1,25 +1,18 @@
 const routes = [
-	{ name: '404', path: '**', template: './components/404.html' },
-	{ name: 'Home', path: '/', template: './components/home.html' },
-	{ name: 'Globals', path: '/globals', template: './components/globals.html' },
-	{ name: 'Buttons', path: '/buttons', template: './components/buttons.html' }
+	{ title: '404', path: '#404', template: './components/404.html' },
+	{ title: 'Home', path: '/', template: './components/home.html' },
+	{ title: 'Globals', path: '#globals', template: './components/globals.html' },
+	{ title: 'Buttons', path: '#buttons', template: './components/buttons.html' }
 ]
 
-const route = (event) => {
-	event = event || window.event
-	event.preventDefault()
-	window.history.pushState({}, '', event.target.href)
-	handleLocation()
-}
-
-const handleLocation = async () => {
-	const path = window.location.pathname
-	const route = routes.find((item) => item.path === path)
+const locationHandler = async () => {
+	const path = window.location.hash || '/'
+	console.log(path)
+	const route = routes.find((item) => item.path === path) || routes[0]
 	const html = await fetch(route.template).then((data) => data.text())
 	document.getElementById('app-root').innerHTML = html
+	document.title = route.title
 }
 
-window.onpopstate = handleLocation
-window.route = route
-
-handleLocation()
+window.addEventListener('hashchange', locationHandler)
+locationHandler()
