4a7e1b6669792c8795f64547dc810cbf5f1e0d18
Luuk <luukbrauckmann@gmail.com>
2022-11-19 17:57:53 +0100

    Reload home fix

--- scripts/router.js
+++ scripts/router.js
@@ -1,12 +1,12 @@
 const routes = [
 	{ title: '404', path: '#404', template: './components/404.html' },
-	{ title: 'Home', path: '/', template: './components/home.html' },
+	{ title: 'Home', path: '#home', template: './components/home.html' },
 	{ title: 'Globals', path: '#globals', template: './components/globals.html' },
 	{ title: 'Buttons', path: '#buttons', template: './components/buttons.html' }
 ]
 
 const locationHandler = async () => {
-	const path = window.location.hash || '/'
+	const path = window.location.hash || '#home'
 	const route = routes.find((item) => item.path === path) || routes[0]
 	const html = await fetch(route.template).then((data) => data.text())
 	document.getElementById('app-root').innerHTML = html
